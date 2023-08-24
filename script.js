c1b8ae4e1af9b2c9376af99d20ee1ea2da2f7601
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-24 23:26:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 //your JS code here. If required.
-const line=document.getElementById("line");
-let rotation=0;
-
-function rotateLine(){
-	rotation+=1;
-	line.style.transform='rotate(${rotaion}deg)';
-	requestAnimationFrame(rotateLine);
-}
-rotateLine();
